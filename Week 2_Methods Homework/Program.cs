b15a70b94819d82b9dcfdfd8c51be8a4d196c5ef
briandyko <briandyko@gmail.com>
2017-10-04 21:06:59 -0400

    Completed method work

--- Week 2_Methods Homework/Program.cs
+++ Week 2_Methods Homework/Program.cs
@@ -10,6 +10,31 @@ namespace Week_2_Methods_Homework
     {
         static void Main(string[] args)
         {
+            FavoriteFood("Brian", "Pasta");
+            Console.WriteLine("Please enter your age to figure out how long until you retire.");
+            RetirementCalculator(0);
+            Console.WriteLine("Your monthly wage is $" + WageCalculator(10, 8.75));
         }
+
+        public static void FavoriteFood(string myName, string favFood)
+        {
+            Console.WriteLine(myName + " " + favFood);
+            return;
+        }
+
+        public static void RetirementCalculator(int userAge)
+        {
+            userAge = int.Parse(Console.ReadLine());
+            int retirementAge = 65 - userAge;
+            Console.WriteLine("The user will retire in " + retirementAge + " years.");
+            return;
+        }
+
+        public static double WageCalculator(double hoursWorked, double hourlyWage)
+        {
+            double monthlyWage = hoursWorked * hourlyWage;
+            return monthlyWage;
+        }
+
     }
 }
